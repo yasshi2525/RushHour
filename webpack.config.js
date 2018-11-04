4318790affe2e3f400aeed544cb8b2348814ff7e
yasshy2 <yasshy2@gmail.com>
2018-11-04 23:49:44 +0900

    react導入

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,27 @@ const path = require('path');
 module.exports = {
     watch: true,
     mode: 'development',
-    entry: './web/index.js',
+    entry: {
+        index: ['./web/index.js', './web/game.jsx']
+    },
     output: {
         path: path.join(__dirname, 'public/js'),
-        filename: 'bundle.js',
+        filename: 'bundle.[name].js',
     },
     module: {
         rules: [
-          {
-            // 対象となるファイルの拡張子(cssのみ)
-            test: /\.css$/,
-            // Sassファイルの読み込みとコンパイル
-            use: [
-              // スタイルシートをJSからlinkタグに展開する機能
-              'style-loader',
-              // CSSをバンドルするための機能
-              'css-loader',
-            ],
-          },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },      
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
         ],
+    }, 
+    resolve: {
+        extensions: ['.js', '.jsx']
     },
 };
