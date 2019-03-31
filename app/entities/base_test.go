d8b4844b05c880d9af3cea2b54b4480b1a395b9c
yasshi2525 <yasshy2@gmail.com>
2019-03-31 13:49:00 +0900

    baseのテスト実装

--- app/entities/base_test.go
+++ app/entities/base_test.go
@@ -5,8 +5,42 @@ import (
 	"time"
 )
 
+func TestNewNase(t *testing.T) {
+	m := NewModel()
+	t.Run("without owner", func(t *testing.T) {
+		b := m.NewBase(COMPANY)
+		if got := b.Idx(); got == 0 {
+			t.Errorf("id should be > 0, but got = %d", got)
+		}
+		if got := b.M; got != m {
+			t.Errorf("M should be %v, but got = %v", m, got)
+		}
+		if got := b.Type(); got != COMPANY {
+			t.Errorf("T should be COMPANY, but got = %v", got)
+		}
+		if got := b.O; got != nil {
+			t.Errorf("O should be nil, but got = %v", b.O)
+		}
+		if got := b.OwnerID; got != ZERO {
+			t.Errorf("OwnerID should be 0, but got = %d", b.OwnerID)
+		}
+		if got := b.ChangedAt; got == *new(time.Time) {
+			t.Errorf("ChangedAt should be > 0, but got = %v", got)
+		}
+	})
+	t.Run("with owner", func(t *testing.T) {
+		o := m.NewPlayer()
+		b := m.NewBase(COMPANY, o)
+		if b.O == nil {
+			t.Errorf("O should not be nil, but got = nil")
+		}
+		if b.OwnerID == ZERO {
+			t.Errorf("OwnerID should not be 0, but got = %d", ZERO)
+		}
+	})
+}
+
 func TestPermits(t *testing.T) {
-	InitType()
 	m := NewModel()
 	my, oth, guest, admin := m.NewPlayer(), m.NewPlayer(), m.NewPlayer(), m.NewPlayer()
 	my.Level, oth.Level, guest.Level, admin.Level = Normal, Normal, Guest, Admin
@@ -33,7 +67,6 @@ func TestPermits(t *testing.T) {
 }
 
 func TestIsChanged(t *testing.T) {
-	InitType()
 	m := NewModel()
 	b := m.NewBase(RESIDENCE)
 
