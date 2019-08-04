13a0f0a57db97e0302b7089515fb5247813b6cb9
yasshi2525 <yasshy2@gmail.com>
2019-08-04 21:34:08 +0900

    拡大時、代表点の移動をスムーズに変更

--- web/common/models/rail.ts
+++ web/common/models/rail.ts
@@ -3,6 +3,7 @@ import { Monitorable, MonitorContrainer } from "../interfaces/monitor";
 import { ApplicationProperty, AnimatedSpriteProperty } from "../interfaces/pixi";
 import { AnimatedSpriteModel, AnimatedSpriteContainer } from "./sprite";
 import { GraphicsAnimationGenerator, GradientAnimationGenerator } from "./animate";
+import { config } from "../interfaces/gamemap";
 
 const graphicsOpts = {
     padding: 10,
@@ -12,7 +13,17 @@ const graphicsOpts = {
     slide: 10
 };
 
-const rnDefaultValues: {color: number} = {color: 0};
+const rnDefaultValues: {
+    pid: number,
+    px: number,
+    py: number,
+    color: number
+} = {
+    pid: 0,
+    px: 0,
+    py: 0,
+    color: 0
+};
 
 export class RailNode extends AnimatedSpriteModel implements Monitorable {
     constructor(options: AnimatedSpriteProperty) {
@@ -22,6 +33,14 @@ export class RailNode extends AnimatedSpriteModel implements Monitorable {
     setupDefaultValues() {
         super.setupDefaultValues();
         this.addDefaultValues(rnDefaultValues);
+    }
+
+    setInitialValues(props: {[index: string]: {}}) {
+        super.setInitialValues(props);
+        if (this.props.pid !== 0) {
+            this.current = this.toView(this.props.px, this.props.py);
+            this.latency = config.latency;
+        }
     }
 
     setupBeforeCallback() {
