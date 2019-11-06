d60322431358539ae58ce5e9b2eaf7ca5f0ab5df
yasshi2525 <yasshy2@gmail.com>
2019-11-06 12:16:21 +0900

    Dockerfileをgin対応化

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-BASEDIR="/rushhour/src/github.com/yasshi2525/RushHour/conf"
+BASEDIR="/rushhour/config"
 
 sed -i -e "s/__ADMIN_USERNAME__/${admin_username}/" ${BASEDIR}/secret.conf
 sed -i -e "s/__ADMIN_PASSWORD__/${admin_password}/" ${BASEDIR}/secret.conf
@@ -9,6 +9,7 @@ sed -i -e "s/__SALT__/${salt}/" ${BASEDIR}/secret.conf
 sed -i -e "s/______KEY_______/${key}/" ${BASEDIR}/secret.conf
 sed -i -e "s/__STATE__/${state}/" ${BASEDIR}/secret.conf
 sed -i -e "s/__COOKIE__/${cookie}/" ${BASEDIR}/secret.conf
+sed -i -e "s|^spec = .*$|spec = \"${db_spec}\"|" ${BASEDIR}/secret.conf
 sed -i -e "s/__TWITTER_TOKEN__/${twitter_token}/" ${BASEDIR}/secret.conf
 sed -i -e "s/__TWITTER_SECRET__/${twitter_secret}/" ${BASEDIR}/secret.conf
 sed -i -e "s/__GOOGLE_CLIENT__/${google_client}/" ${BASEDIR}/secret.conf
@@ -16,4 +17,4 @@ sed -i -e "s/__GOOGLE_SECRET__/${google_secret}/" ${BASEDIR}/secret.conf
 sed -i -e "s/__GITHUB_CLIENT__/${github_client}/" ${BASEDIR}/secret.conf
 sed -i -e "s/__GITHUB_SECRET__/${github_secret}/" ${BASEDIR}/secret.conf
 
-./run.sh
+./RushHour
