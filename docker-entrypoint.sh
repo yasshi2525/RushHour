1abca11428355c044ee715d35c072dc76b0c5e27
yasshi2525 <yasshy2@gmail.com>
2019-11-15 15:30:01 +0900

    db_specが反映されない問題を修正

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,12 +5,12 @@ BASEDIR="/rushhour/config"
 sed -i -e "s/^enabled *= *.*/enabled = ${persist:false}/1" ${BASEDIR}/game.conf
 sed -i -e "s/__ADMIN_USERNAME__/${admin_username}/" ${BASEDIR}/secret.conf
 sed -i -e "s/__ADMIN_PASSWORD__/${admin_password}/" ${BASEDIR}/secret.conf
-sed -i -e "s|^baseurl = .*$|baseurl = \"${baseurl}\"|" ${BASEDIR}/secret.conf
+sed -i -e "s|^baseurl *= *.*$|baseurl = \"${baseurl}\"|" ${BASEDIR}/secret.conf
 sed -i -e "s/__SALT__/${salt}/" ${BASEDIR}/secret.conf
 sed -i -e "s/______KEY_______/${key}/" ${BASEDIR}/secret.conf
 sed -i -e "s/__STATE__/${state}/" ${BASEDIR}/secret.conf
 sed -i -e "s/__COOKIE__/${cookie}/" ${BASEDIR}/secret.conf
-sed -i -e "s|^spec *= *(.*)?$|spec = \"${db_spec}\"|" ${BASEDIR}/secret.conf
+sed -i -e "s|^spec *= *.*$|spec = \"${db_spec}\"|" ${BASEDIR}/secret.conf
 sed -i -e "s/__TWITTER_TOKEN__/${twitter_token}/" ${BASEDIR}/secret.conf
 sed -i -e "s/__TWITTER_SECRET__/${twitter_secret}/" ${BASEDIR}/secret.conf
 sed -i -e "s/__GOOGLE_CLIENT__/${google_client}/" ${BASEDIR}/secret.conf
